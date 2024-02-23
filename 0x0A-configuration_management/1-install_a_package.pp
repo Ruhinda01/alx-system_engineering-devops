ff576bb056bbbb4ea7bd3b1420b212473e87e6ec
RuhindaRoderick Izooba <rickyruhinda@gmail.com>
2024-02-23 13:27:06 -0800

    fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,15 @@
-#!/usr/bin/pup
 # Installs flask from pip3
+
+package { 'python3.8':
+  ensure => present,
+}
 
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip3'
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3'
 }
