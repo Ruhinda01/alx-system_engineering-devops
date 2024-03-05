25b1514ec84060362d187326455340352d047146
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2024-03-04 17:41:55 -0800

    fix

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,12 +1,10 @@
 # Automate the task of creating a custom HTTP header response
 
 exec {'configure_nginx':
-  command  => @(EOT),
-     sudo apt-get -y update
-     sudo apt-get -y install nginx
-     sudo sed -i '/server_name _;/ a\\tadd_header X-Served-By $HOSTNAME;' /etc/nginx/sites-available/default
-     sudo nginx -t
-     sudo service nginx restart
-EOT
+  command  => 'sudo apt-get -y update;
+     sudo apt-get -y install nginx;
+     sudo sed -i '/server_name _;/ a\\tadd_header X-Served-By $HOSTNAME;' /etc/nginx/sites-available/default;
+     sudo nginx -t;
+     sudo service nginx restart',
   provider => shell
 }
