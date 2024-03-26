a95ec4e07ad97c4a52b2e392ca5ec1ffd04de67d
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2024-03-26 06:45:53 +0300

    fix

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -13,16 +13,15 @@ if __name__ == '__main__':
     r1 = requests.get(url + "users/")
     users = r1.json()
     user_dict = {}
-    full_list = []
     for user in users:
         r2 = requests.get(url + "todos", params={user.get("id"): id})
         todo_list = r2.json()
         uname = user.get("username")
+        full_list = []
         for task in todo_list:
             full_list.append({"task": task.get("title"),
                               "completed": task.get("completed"),
                               "username": uname})
         user_dict[user.get("id")] = full_list
-        full_list = []
     with open(file, "w") as f:
         json.dump(user_dict, f)
