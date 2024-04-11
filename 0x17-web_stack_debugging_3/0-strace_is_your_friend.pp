f127289314b977294e2f039920b24c94a7583c8d
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2024-04-12 01:53:43 +0300

    readme

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
 # Automating the fixing of the 500 error
-
-exec { 'replace_phpp_in_settings':
+exec { 'fix-settings':
   command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
-  path    => ['/usr/bin', '/usr/sbin', '/bin'],
+  path    => '/usr/local/bin/:/bin/'
 }
