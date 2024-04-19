4b54a94efe6e50461c0c8c4c9099dc26885cdd6a
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2024-04-19 03:28:13 +0300

    user_limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,10 @@
 # Enables login with the holberton user and open a file
 exec { 'increase-hard-file-limit':
-  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  command => 'sed -i "/holberton hard/s/5/30000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
 
 exec { 'increase-soft-file-limit':
-  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  command => 'sed -i "/holberton soft/s/4/30000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
